6435ee4f496caa30ad0c9bc412808a4583982678
Djalminhagod <r.senra@liceolapaz.net>
2026-04-16 12:37:52 +0200

    ejercicio 3 acabado

--- src/edu/collections/ejercicio3/Principal.java
+++ src/edu/collections/ejercicio3/Principal.java
@@ -1,4 +1,20 @@
 package edu.collections.ejercicio3;
 
+
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Principal {
+    public static void main(String[] args) {
+        Map<String,Integer> inventario = new TreeMap<String,Integer>();
+
+        inventario.put("anillo",2);
+        inventario.put("pulsera",1);
+        inventario.put("piercing",3);
+        inventario.merge("anillo",1, Integer::sum);
+        System.out.println("este es el inventario: "+inventario);
+        inventario.remove("pulsera");
+        System.out.println("este es el inventario: "+inventario);
+
+    }
 }
